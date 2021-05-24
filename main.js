2bcdccc8dacbd0b36168e524370f6738361ed02e
Lakshya Dev <lakshyadev@live.com>
2021-05-25 03:48:42 +0530

    Updated try out calls

--- main.js
+++ main.js
@@ -2,15 +2,22 @@
 const my_db = require('./My_Custom_DB');
 
 // Calls
+
+console.log('\n----------------------------------------------------------');
+console.log('Trying out Function 1: \n');
 try_function_1();
+
+console.log('\n----------------------------------------------------------');
+console.log('Trying out Function 2: \n');
 try_function_2();
+
+console.log('\n----------------------------------------------------------');
+console.log('Trying out Function 3: \n');
 try_function_3();
 
 // Test function_1
 function try_function_1() {
   try {
-    console.log('\n----------------------------------------------------------');
-    console.log('Trying out Function 1: \n');
     console.log(my_db.function_1('2021-05-22')); // Expected O/P: [ 'user_1', 'user_2' ]
     console.log(my_db.function_1('2021-05-21')); // Expected O/P: [ 'user_2' ]
     console.log(my_db.function_1()); // Expected O/P: [ 'user_1', 'user_2' ]
@@ -22,8 +29,6 @@ function try_function_1() {
 
 // Test function_2
 function try_function_2() {
-  console.log('\n----------------------------------------------------------');
-  console.log('Trying out Function 2: \n');
   try {
     console.log(my_db.function_2(3.4, 5)); // Expected O/P: [ 'user_1', 'user_2' ]
     console.log(my_db.function_2(3.3, 5)); //  Expected O/P: [ 'user_2' ]
@@ -35,8 +40,6 @@ function try_function_2() {
 
 // Test function_3
 function try_function_3() {
-  console.log('\n----------------------------------------------------------');
-  console.log('Trying out Function 3: \n');
   try {
     console.log(my_db.function_3('fieldId_1', '2021-05-21', '2021-05-29')); // Expected O/P: 0
     console.log(my_db.function_3('fieldId_1', '2021-05-20', '2021-05-30')); // Expected O/P: 2
